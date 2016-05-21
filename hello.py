50627c722bb900020c20665f2c4c868929640196
ruslannm <brak@mail.ru>
2016-05-21 08:13:59 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 def app(environ, start_response): 
-     start_response('200 OK', [('Content-Type', 'text\plain')]) 
-     qs = "\r\n".join(environ['QUERY_STRING'].split("&"))
-     return qs
+     # start_response('200 OK', [('Content-Type', 'text\plain')]) 
+    # qs = "\r\n".join(environ['QUERY_STRING'].split("&"))
+    # return qs
+     
+    url=env['QUERY_STRING'] 
+    print('uRL ',url) 
+    start_response('200 OK', [('Content-Type', 'text/plain')]) 
+    return [url.replace('&','\n')] 
+
