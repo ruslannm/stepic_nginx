1fe3a8219f8915546945b2e70690d06a7685b0be
ruslannm <brak@mail.ru>
2016-05-21 06:36:06 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
-sudo rm /etc/nginx/sites-enabled/default 
+# sudo rm /etc/nginx/sites-enabled/default 
 sudo /etc/init.d/nginx restart 
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test 
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi -D
+# sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi -D
