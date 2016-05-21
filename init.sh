ef5c4e474e346a75a03e175a0836381f35d4cf6c
ruslannm <brak@mail.ru>
2016-05-21 08:26:39 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
-sudo rm /etc/nginx/sites-enabled/default 
-sudo /etc/init.d/nginx restart 
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test 
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
+#sudo rm /etc/nginx/sites-enabled/default 
+#sudo /etc/init.d/nginx restart 
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test 
 # sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi -D
+
+#!/bin/bash
+
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
