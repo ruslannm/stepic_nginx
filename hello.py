48bf74e904f71d33cbfd1d04cc99d49aa54d5e4d
ruslannm <brak@mail.ru>
2016-05-21 08:28:58 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,28 @@
-def app(environ, start_response): 
+from urlparse import parse_qs
+
+
+def application(environ, start_response):
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    body = []
+    for key, value in query.items():
+        for item in value:
+            body.append(key + "=" + item + "\r\n")
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+
+    start_response(status, headers)
+    return body
+
+# def app(environ, start_response): 
      # start_response('200 OK', [('Content-Type', 'text\plain')]) 
     # qs = "\r\n".join(environ['QUERY_STRING'].split("&"))
     # return qs
      
-    url=env['QUERY_STRING'] 
-    print('uRL ',url) 
-    start_response('200 OK', [('Content-Type', 'text/plain')]) 
-    return [url.replace('&','\n')] 
+#    url=env['QUERY_STRING'] 
+#    print('uRL ',url) 
+#    start_response('200 OK', [('Content-Type', 'text/plain')]) 
+#    return [url.replace('&','\n')] 
 
