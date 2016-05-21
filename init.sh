2625e65c2563b126ac3c4fa088f3d4907fa32722
ruslannm <brak@mail.ru>
2016-05-21 06:29:02 +0400

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
+sudo rm /etc/nginx/sites-enabled/default 
+sudo /etc/init.d/nginx restart 
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test 
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi -D
