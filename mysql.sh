7b4662ec9c22272b933474af055afbbdb1777e1c
ruslannm <brak@mail.ru>
2016-05-28 08:08:43 +0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+mysql -uroot -e "CREATE USER 'admin'@'localhost'"
+mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('pass111')"
+mysql -uroot -e "CREATE DATABASE mybase"
+mysql -uroot -e "GRANT ALL ON mybase.* TO 'admin'@'localhost'"
+
 mysql -uroot -e "CREATE DATABASE stepic_web;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
